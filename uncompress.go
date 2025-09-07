8e4c9c95547e49d560cc68ca9dbc63a2d89aba84
lsq51201314 <lsq51201314@126.com>
2025-09-08 07:05:17 +0800

    Initial commit

--- uncompress.go
+++ uncompress.go
@@ -1,8 +1,9 @@
 package main
 
-import (
-	"encoding/binary"
-)
+import "encoding/binary"
+
+const DECOMP_ERROR = -1
+const MPPE_HIST_LEN = 8192
 
 func Decompress(buffer []byte, dataSize int) []byte {
 	ptr2 := make([]byte, 102400)
@@ -26,272 +27,270 @@ func Decompress(buffer []byte, dataSize int) []byte {
 }
 
 func uncompress(dest []byte, destLen *int, source []byte, sourceLen int) int {
-	num := mppcDecompress(source, dest, sourceLen, *destLen)
-	if num > 0 && num <= *destLen {
-		*destLen = num
+	len := mppcDecompress(source, dest, sourceLen, *destLen)
+	if len > 0 && len <= *destLen {
+		*destLen = len
 		return 0
 	}
 	return -1
 }
 
 func uncompress2(dest []byte, destLen *int, source []byte, sourceLen int) int {
-	num := *destLen
+	dleft := *destLen
 	*destLen = 0
-	var destIndex int = 0
-	var sourceIndex int = 0
+	destIdx := 0
+	sourceIdx := 0
 
-	for sourceLen > 2 && num > 0 {
-		num2 := int(binary.LittleEndian.Uint16(source[sourceIndex:])) & 0x7FFF
-		if num2 <= 0 || num2+2 > sourceLen || num2 > 8192 {
+	for sourceLen > 2 && dleft > 0 {
+		slen := int(binary.LittleEndian.Uint16(source[sourceIdx:])) & 0x7FFF
+		if slen <= 0 || slen+2 > sourceLen || slen > 8192 {
 			return -1
 		}
 
-		num3 := 0
-
-		if (binary.LittleEndian.Uint16(source[sourceIndex:]) & 0x8000) != 0 {
-			num3 = mppcDecompress(source[sourceIndex+2:sourceIndex+2+num2], dest[destIndex:], num2, num)
-			if num3 <= 0 || num3 > num || num3 > 8192 {
+		var dlen int
+		if (binary.LittleEndian.Uint16(source[sourceIdx:]) & 0x8000) != 0 {
+			// 压缩数据
+			dlen = mppcDecompress(source[sourceIdx+2:], dest[destIdx:], slen, dleft)
+			if dlen <= 0 || dlen > dleft || dlen > 8192 {
 				return -1
 			}
 		} else {
-			num3 = num2
-			if num3 > num {
+			// 未压缩数据
+			dlen = slen
+			if dlen > dleft {
 				return -1
 			}
-			copy(dest[destIndex:], source[sourceIndex+2:sourceIndex+2+num3])
+			customFastMemoryCopy(dest[destIdx:], source[sourceIdx+2:], dlen)
 		}
 
-		sourceIndex += num2 + 2
-		sourceLen -= num2 + 2
-		destIndex += num3
-		num -= num3
-		*destLen += num3
+		sourceIdx += slen + 2
+		sourceLen -= slen + 2
+		destIdx += dlen
+		dleft -= dlen
+		*destLen += dlen
 	}
 
-	if sourceLen != 0 {
-		return -1
+	if sourceLen == 0 {
+		return 0
 	}
-	return 0
+	return -1
 }
 
-func mppcDecompress(ibuf []byte, obuf []byte, isize int, osize int) int {
-	if isize > 9217 {
-		return -1
+func mppcDecompress(ibuf, obuf []byte, isize, osize int) int {
+	if isize > (MPPE_HIST_LEN*9)/8+1 {
+		return DECOMP_ERROR
 	}
 
-	ptr := make([]byte, 16384)
-	copy(ptr, ibuf)
-	ibuf = ptr
+	input := make([]byte, 2*MPPE_HIST_LEN)
+	copy(input, ibuf)
+	ibuf = input
 
-	ptr2 := 0
+	obegin := 0
+	oend := osize
+	blenTotal := isize * 8
+	l := 0
+	blen := 7
+	ibufIdx := 0
+	obufIdx := 0
 
-	num := uint32(isize * 8)
-	l := uint32(0)
-	blen := uint32(7)
-	bufIndex := 0
-	currentObufIndex := 0
+	for blenTotal > blen {
+		// 获取位值
+		val := fetch(ibuf, &ibufIdx, &l)
 
-	for num > blen {
-		num2 := fetch(ibuf, &bufIndex, &l)
-
-		if num2 < 0x80000000 {
-			if currentObufIndex >= len(obuf) {
-				return -1
+		if val < 0x80000000 {
+			if obufIdx >= oend {
+				return DECOMP_ERROR
 			}
-			obuf[currentObufIndex] = byte(num2 >> 24)
-			currentObufIndex++
+			obuf[obufIdx] = byte(val >> 24)
+			obufIdx++
 			passbits(8, &l, &blen)
 			continue
 		}
 
-		if num2 < 0xC0000000 {
-			if currentObufIndex >= len(obuf) {
-				return -1
+		if val < 0xc0000000 {
+			if obufIdx >= oend {
+				return DECOMP_ERROR
 			}
-			obuf[currentObufIndex] = byte(((num2 >> 23) | 0x80) & 0xFF)
-			currentObufIndex++
+			obuf[obufIdx] = byte(((val >> 23) | 0x80) & 0xff)
+			obufIdx++
 			passbits(9, &l, &blen)
 			continue
 		}
 
-		var num3, num4 uint32
-
-		if num2 >= 0xF0000000 {
-			num3 = (num2 >> 22) & 0x3F
-			num2 <<= 10
-
-			if num2 < 0x80000000 {
-				num4 = 3
+		var off, len uint32
+		if val >= 0xf0000000 {
+			off = (val >> 22) & 0x3f
+			val <<= 10
+			if val < 0x80000000 {
+				len = 3
 				passbits(11, &l, &blen)
-			} else if num2 < 0xC0000000 {
-				num4 = 4 | ((num2 >> 28) & 3)
+			} else if val < 0xc0000000 {
+				len = 4 | ((val >> 28) & 3)
 				passbits(14, &l, &blen)
-			} else if num2 < 0xE0000000 {
-				num4 = 8 | ((num2 >> 26) & 7)
+			} else if val < 0xe0000000 {
+				len = 8 | ((val >> 26) & 7)
 				passbits(16, &l, &blen)
-			} else if num2 < 0xF0000000 {
-				num4 = 0x10 | ((num2 >> 24) & 0xF)
+			} else if val < 0xf0000000 {
+				len = 16 | ((val >> 24) & 15)
 				passbits(18, &l, &blen)
-			} else if num2 < 0xF8000000 {
-				num4 = 0x20 | ((num2 >> 22) & 0x1F)
+			} else if val < 0xf8000000 {
+				len = 32 | ((val >> 22) & 0x1f)
 				passbits(20, &l, &blen)
-			} else if num2 < 0xFC000000 {
-				num4 = 0x40 | ((num2 >> 20) & 0x3F)
+			} else if val < 0xfc000000 {
+				len = 64 | ((val >> 20) & 0x3f)
 				passbits(22, &l, &blen)
-			} else if num2 < 0xFE000000 {
-				num4 = 0x80 | ((num2 >> 18) & 0x7F)
+			} else if val < 0xfe000000 {
+				len = 128 | ((val >> 18) & 0x7f)
 				passbits(24, &l, &blen)
 			} else {
 				passbits(10, &l, &blen)
-				num2 = fetch(ibuf, &bufIndex, &l)
-
-				if num2 < 0xFF000000 {
-					num4 = 0x100 | ((num2 >> 16) & 0xFF)
+				val = fetch(ibuf, &ibufIdx, &l)
+				if val < 0xff000000 {
+					len = 256 | ((val >> 16) & 0xff)
 					passbits(16, &l, &blen)
-				} else if num2 < 0xFF400000 {
-					num4 = 0x200 | ((num2 >> 14) & 0x1FF)
+				} else if val < 0xff800000 {
+					len = 0x200 | ((val >> 14) & 0x1ff)
 					passbits(18, &l, &blen)
-				} else if num2 < 0xFF600000 {
-					num4 = 0x400 | ((num2 >> 12) & 0x3FF)
+				} else if val < 0xffc00000 {
+					len = 0x400 | ((val >> 12) & 0x3ff)
 					passbits(20, &l, &blen)
-				} else if num2 < 0xFF700000 {
-					num4 = 0x800 | ((num2 >> 10) & 0x7FF)
+				} else if val < 0xffe00000 {
+					len = 0x800 | ((val >> 10) & 0x7ff)
 					passbits(22, &l, &blen)
-				} else {
-					if num2 >= 0xFF780000 {
-						return -1
-					}
-					num4 = 0x1000 | ((num2 >> 8) & 0xFFF)
+				} else if val < 0xfff00000 {
+					len = 0x1000 | ((val >> 8) & 0xfff)
 					passbits(24, &l, &blen)
+				} else {
+					return DECOMP_ERROR
 				}
 			}
-		} else if num2 >= 0xE0000000 {
-			num3 = ((num2 >> 20) & 0xFF) + 64
-			num2 <<= 12
-
-			if num2 < 0x80000000 {
-				num4 = 3
+		} else if val >= 0xe0000000 {
+			off = ((val >> 20) & 0xff) + 64
+			val <<= 12
+			if val < 0x80000000 {
+				len = 3
 				passbits(13, &l, &blen)
-			} else if num2 < 0xC0000000 {
-				num4 = 4 | ((num2 >> 28) & 3)
+			} else if val < 0xc0000000 {
+				len = 4 | ((val >> 28) & 3)
 				passbits(16, &l, &blen)
-			} else if num2 < 0xE0000000 {
-				num4 = 8 | ((num2 >> 26) & 7)
+			} else if val < 0xe0000000 {
+				len = 8 | ((val >> 26) & 7)
 				passbits(18, &l, &blen)
-			} else if num2 < 0xF0000000 {
-				num4 = 0x10 | ((num2 >> 24) & 0xF)
+			} else if val < 0xf0000000 {
+				len = 16 | ((val >> 24) & 15)
 				passbits(20, &l, &blen)
-			} else if num2 < 0xF8000000 {
-				num4 = 0x20 | ((num2 >> 22) & 0x1F)
+			} else if val < 0xf8000000 {
+				len = 32 | ((val >> 22) & 0x1f)
 				passbits(22, &l, &blen)
-			} else if num2 < 0xFC000000 {
-				num4 = 0x40 | ((num2 >> 20) & 0x3F)
+			} else if val < 0xfc000000 {
+				len = 64 | ((val >> 20) & 0x3f)
 				passbits(24, &l, &blen)
 			} else {
 				passbits(12, &l, &blen)
-				num2 = fetch(ibuf, &bufIndex, &l)
-
-				if num2 < 0xFE000000 {
-					num4 = 0x80 | ((num2 >> 18) & 0x7F)
+				val = fetch(ibuf, &ibufIdx, &l)
+				if val < 0xfe000000 {
+					len = 128 | ((val >> 18) & 0x7f)
 					passbits(14, &l, &blen)
-				} else if num2 < 0xFF000000 {
-					num4 = 0x100 | ((num2 >> 16) & 0xFF)
+				} else if val < 0xff000000 {
+					len = 256 | ((val >> 16) & 0xff)
 					passbits(16, &l, &blen)
-				} else if num2 < 0xFF400000 {
-					num4 = 0x200 | ((num2 >> 14) & 0x1FF)
+				} else if val < 0xff800000 {
+					len = 0x200 | ((val >> 14) & 0x1ff)
 					passbits(18, &l, &blen)
-				} else if num2 < 0xFF600000 {
-					num4 = 0x400 | ((num2 >> 12) & 0x3FF)
+				} else if val < 0xffc00000 {
+					len = 0x400 | ((val >> 12) & 0x3ff)
 					passbits(20, &l, &blen)
-				} else if num2 < 0xFF700000 {
-					num4 = 0x800 | ((num2 >> 10) & 0x7FF)
+				} else if val < 0xffe00000 {
+					len = 0x800 | ((val >> 10) & 0x7ff)
 					passbits(22, &l, &blen)
-				} else {
-					if num2 >= 0xFF780000 {
-						return -1
-					}
-					num4 = 0x1000 | ((num2 >> 8) & 0xFFF)
+				} else if val < 0xfff00000 {
+					len = 0x1000 | ((val >> 8) & 0xfff)
 					passbits(24, &l, &blen)
+				} else {
+					return DECOMP_ERROR
 				}
 			}
 		} else {
-			num3 = ((num2 >> 16) & 0x1FFF) + 320
-			num2 <<= 16
-
-			if num2 < 0x80000000 {
-				num4 = 3
+			off = ((val >> 16) & 0x1fff) + 320
+			val <<= 16
+			if val < 0x80000000 {
+				len = 3
 				passbits(17, &l, &blen)
-			} else if num2 < 0xC0000000 {
-				num4 = 4 | ((num2 >> 28) & 3)
+			} else if val < 0xc0000000 {
+				len = 4 | ((val >> 28) & 3)
 				passbits(20, &l, &blen)
-			} else if num2 < 0xE0000000 {
-				num4 = 8 | ((num2 >> 26) & 7)
+			} else if val < 0xe0000000 {
+				len = 8 | ((val >> 26) & 7)
 				passbits(22, &l, &blen)
-			} else if num2 < 0xF0000000 {
-				num4 = 0x10 | ((num2 >> 24) & 0xF)
+			} else if val < 0xf0000000 {
+				len = 16 | ((val >> 24) & 15)
 				passbits(24, &l, &blen)
 			} else {
 				passbits(16, &l, &blen)
-				num2 = fetch(ibuf, &bufIndex, &l)
-
-				if num2 < 0xF8000000 {
-					num4 = 0x20 | ((num2 >> 22) & 0x1F)
+				val = fetch(ibuf, &ibufIdx, &l)
+				if val < 0xf8000000 {
+					len = 32 | ((val >> 22) & 0x1f)
 					passbits(10, &l, &blen)
-				} else if num2 < 0xFC000000 {
-					num4 = 0x40 | ((num2 >> 20) & 0x3F)
+				} else if val < 0xfc000000 {
+					len = 64 | ((val >> 20) & 0x3f)
 					passbits(12, &l, &blen)
-				} else if num2 < 0xFE000000 {
-					num4 = 0x80 | ((num2 >> 18) & 0x7F)
+				} else if val < 0xfe000000 {
+					len = 128 | ((val >> 18) & 0x7f)
 					passbits(14, &l, &blen)
-				} else if num2 < 0xFF000000 {
-					num4 = 0x100 | ((num2 >> 16) & 0xFF)
+				} else if val < 0xff000000 {
+					len = 256 | ((val >> 16) & 0xff)
 					passbits(16, &l, &blen)
-				} else if num2 < 0xFF400000 {
-					num4 = 0x200 | ((num2 >> 14) & 0x1FF)
+				} else if val < 0xff800000 {
+					len = 0x200 | ((val >> 14) & 0x1ff)
 					passbits(18, &l, &blen)
-				} else if num2 < 0xFF600000 {
-					num4 = 0x400 | ((num2 >> 12) & 0x3FF)
+				} else if val < 0xffc00000 {
+					len = 0x400 | ((val >> 12) & 0x3ff)
 					passbits(20, &l, &blen)
-				} else if num2 < 0xFF700000 {
-					num4 = 0x800 | ((num2 >> 10) & 0x7FF)
+				} else if val < 0xffe00000 {
+					len = 0x800 | ((val >> 10) & 0x7ff)
 					passbits(22, &l, &blen)
-				} else {
-					if num2 >= 0xFF780000 {
-						return -1
-					}
-					num4 = 0x1000 | ((num2 >> 8) & 0xFFF)
+				} else if val < 0xfff00000 {
+					len = 0x1000 | ((val >> 8) & 0xfff)
 					passbits(24, &l, &blen)
+				} else {
+					return DECOMP_ERROR
 				}
 			}
 		}
 
-		if currentObufIndex-int(num3) < ptr2 || currentObufIndex+int(num4) > len(obuf) {
-			return -1
+		// 复制数据
+		if obufIdx-int(off) < obegin || obufIdx+int(len) > oend {
+			return DECOMP_ERROR
 		}
 
-		lamecopy(obuf[currentObufIndex:], obuf[currentObufIndex-int(num3):], num4)
-		currentObufIndex += int(num4)
+		lamecopy(obuf[obufIdx:], obuf[obufIdx-int(off):], int(len))
+		obufIdx += int(len)
 	}
 
-	return currentObufIndex - ptr2
+	return obufIdx
 }
 
-func passbits(n uint32, l *uint32, blen *uint32) {
+func passbits(n int, l, blen *int) {
 	*l += n
 	*blen += n
 }
 
-func fetch(buf []byte, bufIndex *int, l *uint32) uint32 {
-	*bufIndex += int(*l >> 3)
-	*l &= 7
-
-	val := binary.LittleEndian.Uint32(buf[*bufIndex:])
-	converted := byteorder32(val)
-	return converted << int(*l)
-}
-
-func lamecopy(dst []byte, src []byte, length uint32) {
-	for i := uint32(0); i < length; i++ {
+func lamecopy(dst, src []byte, len int) {
+	for i := 0; i < len; i++ {
 		dst[i] = src[i]
 	}
+}
+
+func customFastMemoryCopy(dest, src []byte, n int) {
+	copy(dest, src[:n])
+}
+
+func fetch(buf []byte, bufIdx *int, l *int) uint32 {
+	*bufIdx += *l >> 3
+	*l &= 7
+	if *bufIdx+4 > len(buf) {
+		return 0
+	}
+	val := binary.LittleEndian.Uint32(buf[*bufIdx:])
+	return byteorder32(val) << *l
 }
